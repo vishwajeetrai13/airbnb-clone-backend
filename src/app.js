a0aba0d044da4b4a0c2b06f3e8605a5642aa193a
vishwajeetrai13 <vishwajeet.rai13@mountblue.tech>
2020-10-19 12:38:42 +0530

    add json format in app

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
-const express=require("express")
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 // listingRouteSample = require("./routes/listingRoute").listingRouteSample;
 import { listingRouteSample } from "./routes/listingRoute";
 let bookingRouteSample = require("./routes/bookingRoute").bookingRouteSample;
-const app = express();
-app.options('*', cors())
-app.set("port", process.env.PORT || 3000)
-app.set("env", process.env.NODE_ENV || "development")
 
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage')
-})
-  
-app.get('/booking', function (req, res) {
-    bookingRouteSample();
-    res.send('booking')
-})
-  
-app.get('/listing', function (req, res) {
-    listingRouteSample();
-    res.send('listing')
-})
+const app = express();
+
+app.options("*", cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("port", process.env.PORT || 3000);
+app.set("env", process.env.NODE_ENV || "development");
+
+app.get("/", function (req, res) {
+  res.send("GET request to the homepage");
+});
+
+app.get("/booking", function (req, res) {
+  bookingRouteSample();
+  res.send("booking");
+});
+
+app.get("/listing", function (req, res) {
+  listingRouteSample();
+  res.send("listing");
+});
 
 module.exports = app;
--- src/models/dbTest.js
+++
@@ -1,5 +0,0 @@
-
-	createdAt	updatedAt	email	password	dob	profilePictureUrl
-
-const jane = await User.create({ firstName: "Jane", lastName: "Doe",introductionOfUser:"yo",createdAt:"new",updatedAt:"old",email:"abc@gmail.com",password:"123",dob:"01", profilePictureUrl:""});
-console.log("Jane's auto-generated ID:", jane.id);
