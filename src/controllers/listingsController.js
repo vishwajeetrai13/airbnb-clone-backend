bf8d8093a012895ed90603919bdcf1aa1afb67a6
aquib-J <aquib.jansher@gmail.com>
2020-10-27 15:31:59 +0530

    hotfix listing-reviews

--- src/controllers/listingsController.js
+++ src/controllers/listingsController.js
@@ -1,144 +1,154 @@
-const { review } = require('../model/indexModel');
-const db=require('../model/indexModel');
+const { review } = require("../model/indexModel");
+const db = require("../model/indexModel");
 
-
-const search=async (req,res)=>{
-  try{
-  if(!req.query.city){
-    res.status(400).send('Please Enter a valid city');
-  }
-  else if(req.query.city[0]=="'" || req.query.city[req.query.city.length-1]=="'"){
-    res.status(400).send('Please remove the double-quotes from the query param value')
-  }
-  else{
-    
-    const listings=await db.listing.findAll({
-      include: {
-        model: db.city,
-        attributes: ["cityName"],
-        where: {
-          cityName: req.query.city,
+const search = async (req, res) => {
+  try {
+    if (!req.query.city) {
+      res.status(400).send("Please Enter a valid city");
+    } else if (
+      req.query.city[0] == "'" ||
+      req.query.city[req.query.city.length - 1] == "'"
+    ) {
+      res
+        .status(400)
+        .send("Please remove the double-quotes from the query param value");
+    } else {
+      const listings = await db.listing.findAll({
+        include: {
+          model: db.city,
+          attributes: ["cityName"],
+          where: {
+            cityName: req.query.city,
+          },
         },
-      },
-    });
+      });
 
-    if (listings.length==0){
-    res.status(200).send('we are not yet operational in this city');  
-  }
-
-  const images= await Promise.all(listings.map(async (listing) =>{
-    return await db.listingImage.findAll({
-      attributes:['url'],
-      where:{
-        entityId:listing.id
+      if (listings.length == 0) {
+        res.status(200).send("we are not yet operational in this city");
       }
-    }); 
-}));
 
-const listingsAndImages=[];
+      const images = await Promise.all(
+        listings.map(async listing => {
+          return await db.listingImage.findAll({
+            attributes: ["url"],
+            where: {
+              entityId: listing.id,
+            },
+          });
+        })
+      );
 
-for(let i=0;i<listings.length;i++){
-  let listingCopy=JSON.parse(JSON.stringify(listings));
-  listingCopy[i].images=images[i]
-  listingsAndImages.push(listingCopy[i]);
-}
+      const listingsAndImages = [];
 
-  res.status(200).send(listingsAndImages);
+      for (let i = 0; i < listings.length; i++) {
+        let listingCopy = JSON.parse(JSON.stringify(listings));
+        listingCopy[i].images = images[i];
+        listingsAndImages.push(listingCopy[i]);
+      }
 
-}
+      res.status(200).send(listingsAndImages);
+    }
+  } catch (err) {
+    console.error(err);
   }
-catch(err){
-  console.error(err);
-}
 };
 
+const findById = async (req, res) => {
+  try {
+    if (Number(req.params.id).toString() !== req.params.id) {
+      res.status(400).send(`Please enter a valid id-number`);
+    } else {
+      const listing = await db.listing.findOne({
+        where: {
+          id: Number(req.params.id),
+        },
+      });
 
-const findById=async (req,res)=>{
-    try{
-      if(Number(req.params.id).toString()!==req.params.id){
-        res.status(400).send(`Please enter a valid id-number`);
-      } 
+      if (!listing) {
+        return res.status(400).send(`No Listings exist for this entry`);
+      }
 
-      else{ 
+      const city = await db.city.findOne({
+        where: {
+          id: listing.cityId,
+        },
+      });
 
-        const listing=await db.listing.findOne({
-          where:{
-            id:Number(req.params.id),      
-          },
-        });
-        
-        if(!listing){
-         return res.status(400).send(`No Listings exist for this entry`);
-        }
+      const host = await db.user.findAll({
+        where: {
+          id: listing.hostID,
+        },
+      });
 
-        const city=await db.city.findOne({
-          where:{
-            id:listing.cityId,
-          }
-        });
+      const images = await db.listingImage.findAll({
+        attributes: ["url"],
+        where: {
+          entityId: listing.id,
+        },
+      });
 
-        const host=await db.user.findAll({
-          where:{
-            id:listing.hostID,
-          }
-        });
+      const booking = await db.booking.findAll({
+        where: {
+          listingId: listing.id,
+        },
+      });
 
-        const images=await db.listingImage.findAll({
-          attributes:['url'],
-          where:{
-            entityId:listing.id,
-          }
-        });
-
-        const booking=await db.booking.findAll({
-          where:{
-            listingId:listing.id
-          }
-        })
-
-        const review=await Promise.all(booking.map(async booking => {
+      const review = await Promise.all(
+        booking.map(async booking => {
           return await db.review.findOne({
-          where:{
-            bookingId:booking.id
-          }
-        });
-      }));
-
-      const reviewBy=await Promise.all(booking.map(async booking=>{
-        return await db.user.findOne({
-          where:{
-            id:booking.userId
-          }
+            where: {
+              bookingId: booking.id,
+            },
+          });
         })
-      }));
+      );
+
+      const reviewBy = await Promise.all(
+        booking.map(async booking => {
+          return await db.user.findOne({
+            where: {
+              id: booking.userId,
+            },
+          });
+        })
+      );
 
       let rev;
-      if(review){
-       rev=review.map((rev,i)=>{
-         rev=JSON.parse(JSON.stringify(rev))
-        rev[`reviewerInfo`]={firstName:reviewBy[i].firstName,lastName:reviewBy[i].lastName,pic:reviewBy[i].profilePictureUrl}
-        return rev;
-      });
-    }else{
-      rev=review;
-    }
+      if ((review.includes(null) && review.length!=1) || review) {
+        rev = review.map((rev, i) => {
+          rev = JSON.parse(JSON.stringify(rev));
+          if(rev!=null){
+          rev[`reviewerInfo`] = {
+            firstName: reviewBy[i].firstName,
+            lastName: reviewBy[i].lastName,
+            pic: reviewBy[i].profilePictureUrl,
+          };
+        }
+          return rev;
+        });
+      } else {
+        rev = review;
+      }
+      rev=rev.filter(rev=>rev!=null)
 
+      const deepClone = entity => JSON.parse(JSON.stringify(entity));
 
+      let resObj = {
+        listing: deepClone(listing),
+        city: deepClone(city),
+        host: deepClone(host),
+        images: deepClone(images),
+        bookings: deepClone(booking),
+        review: deepClone(rev),
+        // review: deepClone(review),
+        // reviewBy:deepClone(reviewBy),
 
-     const deepClone=(entity)=> JSON.parse(JSON.stringify(entity));
-        
-        let resObj={listing:deepClone(listing),city:deepClone(city),host:deepClone(host),images:deepClone(images),bookings:deepClone(booking),review:deepClone(rev)}
+      };
       res.status(200).send(resObj);
-    
     }
-
+  } catch (err) {
+    console.error(err);
   }
-    catch(err){
-      console.error(err);
-    }
-
-  
 };
 
-module.exports={search,findById}
-
+module.exports = { search, findById };
